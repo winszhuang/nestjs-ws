1b9583f0734f11929d0321757497dee066052826
wins <win4709hi@gmail.com>
2023-01-25 20:40:43 +0800

    feat: 0.0.0.0

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useWebSocketAdapter(new WsAdapter(app));
-  await app.listen(3000);
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
